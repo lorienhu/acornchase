d26b05a85ac0c40272066ee16d771366241a80eb
Lorien Hu <lorienbh@gmail.com>
2014-02-22 11:10:20 -0800

    adding fields and constructor to Game

--- Acorn-Chase/src/model/Game.java
+++ Acorn-Chase/src/model/Game.java
@@ -1,5 +1,25 @@
 package acornchase;
 
+// Represents an AcornGame.
 public class Game {
+
+	public static final int WIDTH = 800;
+	public static final int HEIGHT = 600;
+	public static final Random RND = new Random();
+	
+	private Squirrel player;
+	private Squirrel enemy;
+	
+	private int time;
+	
+	public String state;
+	
+	public Game() {
+		state = "start";
+		time = 0;
+		player = new Squirrel();
+		enemy = new Squirrel();
+		
+	}
 
 }
