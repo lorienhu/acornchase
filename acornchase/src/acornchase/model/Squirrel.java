a19e5d39ff745cd6cbecc4b2acadb2c582903847
Lorien Hu <lorienbh@gmail.com>
2014-02-22 15:42:33 -0800

    floop

--- acornchase/src/acornchase/model/Squirrel.java
+++ acornchase/src/acornchase/model/Squirrel.java
@@ -12,6 +12,7 @@ public class Squirrel extends Movable {
 		this.x_speed = base_speed;
 		this.y_speed = 0;
 		this.x_position = position;
+		this.y_position = 100;
 		this.colour = col;
 
 	}
@@ -46,9 +47,7 @@ public class Squirrel extends Movable {
 	public void draw(Graphics g) {
 		// TODO Auto-generated method stub
 		System.out.println("got here");
-		Color savedCol = g.getColor();
 		g.setColor(colour);
 		g.fillRect(x_position - WIDTH / 2, y_position - HEIGHT / 2, WIDTH, HEIGHT);
-		g.setColor(savedCol);
 	}
 }
