c4ac2c288826693461e6eb312c38f55bbd44db3f
amelia-ylh <amelia_ylh@hotmail.com>
2014-02-22 12:23:49 -0800

    changes to Drawable and Movable

--- acornchase/src/acornchase/model/Drawable.java
+++ acornchase/src/acornchase/model/Drawable.java
@@ -1,5 +1,15 @@
 package acornchase.model;
 
-public class Drawable {
-
+public abstract class Drawable {
+	
+	
+	protected int x_location;
+	protected int y_location;
+	
+	
+	public void setLocation(int x) {
+		x_location = x;
+	}
+	
+	
 }
--- acornchase/src/acornchase/model/Movable.java
+++ acornchase/src/acornchase/model/Movable.java
@@ -2,4 +2,19 @@ package acornchase.model;
 
 public abstract class Movable {
 
+	private char direction = 'L';
+
+	public void setDirection(char c) {
+		direction = c;
+	}
+	
+	// Moves the squirrel
+	// modifies: this
+	// effects:  squirrel has been moved
+	public void move() {
+		
+	}
+	
+	
 }
+
